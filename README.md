5e7537ec9373f2a2e40765b7a37fdc2f8879fd1c
Nigel Finley <nfinleymusic@gmail.com>
2021-01-25 21:52:34 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# Getting Started with Create React App
+# Tonebirds basic landing site w/google form
+
+
+## Info about the site
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
+import backgroundImage from './assets/background_1.jpg';
+import logo from './assets/logo.png';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+      }}
+    >
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="container">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="iframe">
+            <iframe
+              src="https://docs.google.com/forms/d/e/1FAIpQLSejqwUBCnXTtsHslcPdawc-SysO0qBog1ryFRiRbrzJuxwG9w/viewform?embedded=true"
+              width="640"
+              height="960"
+              frameborder="0"
+              marginheight="0"
+              marginwidth="0"
+              title="tonebirds survery"
+            >
+              Loading…
+            </iframe>
+          </div>
+        </div>
+        <div class="credit">
+          Photo by{' '}
+          <a href="https://unsplash.com/@claybanks?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+            Clay Banks
+          </a>{' '}
+          on{' '}
+          <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+            Unsplash
+          </a>
+        </div>
       </header>
     </div>
   );
